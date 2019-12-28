34996caaa9a78353f830841f793261744e203b84
Debonet <719893090@qq.com>
2019-12-28 21:15:43 +0800

    lazyload

--- webTask3/public/javascripts/index.js
+++ webTask3/public/javascripts/index.js
@@ -1,29 +1,33 @@
-var viewHeight = document.documentElement.clientHeight;
-
-function lazyload() {
-    var eles = document.querySelectorAll('img[data-src][lazyload]');
-    Array.prototype.forEach.call(eles, function(item, index) {
-        var rect;
-        if (item.dataset.src === "") {
-            return;
-        }
-        rect = item.getBoundingClientRect();
-        if (rect.bottom >= 0 && rect.top < viewHeight) {
-            ! function() {
-                var img = new Image();
-                img.src = item.dataset.url;
-                img.onload = function() {
-                    item.src = img.src;
-                }
-                item.removeAttribute("data-src");
-                item.removeAttribute("lazyload");
-            }();
-        }
-    })
+var saved;
+if (typeof window.onload == 'function') {
+    saved = window.onload;
 }
 
 
 window.onload = function() {
-    this.lazyload();
+
+    if (saved)
+        saved();
+
+    var viewHeight = document.documentElement.clientHeight;
+
+    function lazyload() {
+        var eles = document.querySelectorAll('img[data-original][lazyload]');
+        Array.prototype.forEach.call(eles, function(item, index) {
+            var rect;
+            if (item.dataset.original === "") {
+                return;
+            }
+            rect = item.getBoundingClientRect();
+            if (rect.bottom >= 0 && rect.top < viewHeight) {
+                ! function() {
+                    item.src = item.dataset.original;
+                    item.removeAttribute("data-original");
+                    item.removeAttribute("lazyload");
+                }();
+            }
+        })
+    }
+    lazyload();
     document.addEventListener("scroll", lazyload);
 }
